f56ef52223488e98c0e8d02e5fd3d9ed848b6c21
Thato MacBook <thatofreelance@gmail.com>
2023-03-29 14:54:18 +0200

    express setup

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log('Task Manager App')
+
+const express = require('express')
+const app = express()
+
+// app.use(express.static('public'))
+
+app.get('/', (req, res)=> {
+    res.send('we are home')
+})
+
+app.listen(3000, ()=> {
+    console.log('server is listining on port: ', 3000)
+})
