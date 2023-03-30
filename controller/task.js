533008a4dc3e9b809d552809584339f6d5e63350
Thato MacBook <thatofreelance@gmail.com>
2023-03-30 22:37:37 +0200

    cleaned up routes

--- controller/task.js
+++ controller/task.js
@@ -1,38 +1,78 @@
-const task = require('../model/task')
+const taskSchema = require('../model/task')
 
 
 //get specific task
-const getTask = (id) => {
-    return task.findById({_id : id})
+const getTask = async (req, res) => {
+    const id = req.params.id
+    if (!id){
+        return res.status(404).json({error: 'id not found'})
+    }
+    const task = await taskSchema.findById({_id : id})
+    if (!task){
+        return res.status(404).json({error: 'Item not found'})
+    }else {
+         return res.status(200).json({task})
+    } 
 }
 
 //create a task
-const createTask = (tasktoadd)=> {
-    const data = new task({ name: `${tasktoadd.name}` });
-    return  data.save();
+const createTask = async (req, res)=> {
+    const tasktoadd = req.body
+    const saveData = new taskSchema({ name: `${tasktoadd.name}` });
+    const task  = await saveData.save();
+    if (!task){
+        return res.status(404).json({error: 'Item not found'})
+    }else {
+         return res.status(200).json({task})
+    } 
 }
 
 //get all task
-const getAllTask =  ()=> {
-    const data = task.find()
-    return data
-}
-
-//update a task
-const updateTask = (id, body)=> {
-    try {
-        const data = task.updateOne({_id: id}, body)
-        return data;
-    } catch (error) {
-        console.log(error)
-        return "error"
+const getAllTask = async (req, res)=> {
+    const tasks = await  taskSchema.find()
+    if (!tasks){
+        return res.status(404).json({error: 'Item not found'})
+    } else {
+        return res.status(200).json({tasks})
     }
 }
 
+//update a task
+const updateTask = async (req, res) => {
+    const id = req.params.id
+    if (!id){
+        return res.status(404).json({error: 'id not found'})
+    }
+    const data = req.body
+    if (!data){
+        return res.status(404).json({error: 'no data in body'})
+    }
+    const task = await taskSchema.updateOne({_id: id}, data);
+    if (task.n===0){
+        return res.status(404).json({error: 'item not found'})
+    }
+    if (task.nModified===0){
+        return res.status(404).json({error: 'item not changed'})
+    }
+   return res.status(200).json({success: 'item changed'});
+
+}
+
 //delete a task
-const deleteATask = (id)=> {
-    const data = task.deleteOne({_id: id})
-    return data
+const deleteATask = async (req, res)=> {
+    const id = req.params.id
+    if (!id){
+       return  res.status(404).json({error: 'please provide id'})
+    }
+    const task = await taskSchema.deleteOne({_id: id})
+    if (task.n===0){
+        return res.status(404).json({error: 'item not found'})
+    }
+    if (task.nModified===0){
+        return res.status(404).json({error: 'item not deleted'})
+    }else {
+        return  res.status(200).json({success: 'item deleted'});
+    }
 }
 
 module.exports = {
--- routes/task.js
+++ routes/task.js
@@ -4,72 +4,19 @@ const { createTask, getAllTask, deleteATask, updateTask, getTask } = require('..
 
 
 //create a task
-router.post('/api/v1/tasks', (req, res) => {
-    const data = req.body
-    console.log('getting data...', data)
-    const task = data.name
-    createTask(data)
-    res.send(data)
-})
+router.post('/api/v1/tasks', createTask)
 
 //get all tasks
-router.get('/api/v1/tasks', async (req, res) => {
-    const tasks = await getAllTask()    
-    if (!tasks){
-        return res.status(404).json({error: 'Item not found'})
-    } else {
-        return res.status(200).json({tasks})
-    }
-})
+router.get('/api/v1/tasks', getAllTask);
+
 //get specific task
-router.get('/api/v1/tasks/:id', async (req, res) => {
-    const id = req.params.id
-    if (!id){
-        return res.status(404).json({error: 'id not found'})
-    }
-    const task = await getTask(id)
-    if (!task){
-        return res.status(404).json({error: 'Item not found'})
-    }else {
-         return res.status(200).json({task})
-    } 
-})
+router.get('/api/v1/tasks/:id', getTask);
+
 //update a task 
-router.patch('/api/v1/tasks/:id', async(req, res) => {
-    const id = req.params.id
-    if (!id){
-        return res.status(404).json({error: 'id not found'})
-    }
-    const data = req.body
-    if (!data){
-        return res.status(404).json({error: 'no data in body'})
-    }
-    const task = await updateTask(id, data)
-    if (task.n===0){
-        return res.status(404).json({error: 'item not found'})
-    }
-    if (task.nModified===0){
-        return res.status(404).json({error: 'item not changed'})
-    }
-   return  res.status(200).json({success: 'item changed'});
-})
+router.patch('/api/v1/tasks/:id', updateTask);
 
 //delete a task
-router.delete('/api/v1/tasks/:id',async (req, res) => {
-    const id = req.params.id
-    if (!id){
-       return  res.status(404).json({error: 'please provide id'})
-    }
-    const task = await deleteATask(id)
-    if (task.n===0){
-        return res.status(404).json({error: 'item not found'})
-    }
-    if (task.nModified===0){
-        return res.status(404).json({error: 'item not deleted'})
-    }else {
-        return  res.status(200).json({success: 'item deleted'});
-    }
-})
+router.delete('/api/v1/tasks/:id', deleteATask)
 
 
 module.exports = router
